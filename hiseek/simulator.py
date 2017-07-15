6d5dec2989a4ab1d763d64e2a1b3548d381133b2
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-07-15 11:30:16 +0530

    Edited simulator

--- hiseek/simulator.py
+++ hiseek/simulator.py
@@ -1,13 +1,20 @@
 import statistic
 import grid
+import team
+import percept
 
 class Simulator(object):
 	"""
 		Responsible for one complete simulation
 	"""
 
-	def __init__(self, display, mode_hiders, mode_seekers, num_hiders, num_seekers, map_id, input_file, output_file, verbose):
+	mode_type_hiders = ['random']
+	mode_type_seekers = ['random']
+
+	def __init__(self, display, mode_hiders, mode_seekers, num_hiders, num_seekers, map_id, input_file, output_file, verbose, max_steps=1000):
 		self.__display = display
+		assert(mode_hiders in Simulator.mode_type_hiders)
+		assert(mode_seekers in Simulator.mode_type_seekers)
 		self.__mode_hiders = mode_hiders
 		self.__mode_seekers = mode_seekers
 		self.__num_hiders = num_hiders
@@ -17,7 +24,26 @@ class Simulator(object):
 		self.__output_file = output_file
 		self.__verbose = verbose
 		self.__stats = statistic.Statistic(num_hiders, num_seekers)
+		self.__max_steps = max_steps
 
 		self.__map = grid.Map(map_id)
+
+	def simulate(self):
+		hider_map = copy.deepcopy(self.__map)
+		seeker_map = copy.deepcopy(self.__map)
+
+		hiders_caught = 0
+
+		if self.__mode_hiders == 'random':
+			hider_team = team.RandomHiderTeam(self.__num_hiders, hider_map)
+		
+		if self.__mode_seekers == 'random':
+			seeker_team = team.RandomSeekerTeam(self.__num_seekers, seeker_map)
+
+		while hiders_caught < self.__num_hiders and time_step < self.__max_steps:
+			# update the percept of each robot
+
+
+
 
 
