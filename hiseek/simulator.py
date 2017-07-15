4cb45135615a91fcb8d23f3cd4b5a36cc06e3065
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-07-15 22:18:21 +0530

    Edited simulator module

--- hiseek/simulator.py
+++ hiseek/simulator.py
@@ -1,7 +1,13 @@
+import random
+
+import pyglet
+
 import statistic
-import grid
+import gamemap
 import team
 import percept
+import action
+import graphics
 
 class Simulator(object):
 	"""
@@ -11,7 +17,7 @@ class Simulator(object):
 	mode_type_hiders = ['random']
 	mode_type_seekers = ['random']
 
-	def __init__(self, display, mode_hiders, mode_seekers, num_hiders, num_seekers, map_id, input_file, output_file, verbose, max_steps=1000):
+	def __init__(self, display, mode_hiders, mode_seekers, num_hiders, num_seekers, map_id, input_file, output_file, verbose, max_steps=1000, window_width=640, window_height=360):
 		self.__display = display
 		assert(mode_hiders in Simulator.mode_type_hiders)
 		assert(mode_seekers in Simulator.mode_type_seekers)
@@ -25,25 +31,30 @@ class Simulator(object):
 		self.__verbose = verbose
 		self.__stats = statistic.Statistic(num_hiders, num_seekers)
 		self.__max_steps = max_steps
+		self.__map = gamemap.PolygonMap(0)
 
-		self.__map = grid.Map(map_id)
+		self.__window_width = window_width
+		self.__window_height = window_height
+		self.__window = graphics.Graphics(window_width, window_height, num_hiders, num_seekers, self.__map)
+		
+
+	def __update_simulation(self, dt):
+		available_actions = list(action.Action)
+
+		for i in range(self.__num_hiders):
+			act = random.choice(available_actions)
+			self.__window.set_hider_action(i, act)
+
+		for i in range(self.__num_seekers):
+			act = random.choice(available_actions)
+			self.__window.set_seeker_action(i, act)
+		print('Updated')
+		self.__window.update(dt)
 
 	def simulate(self):
-		hider_map = copy.deepcopy(self.__map)
-		seeker_map = copy.deepcopy(self.__map)
-
-		hiders_caught = 0
-
-		if self.__mode_hiders == 'random':
-			hider_team = team.RandomHiderTeam(self.__num_hiders, hider_map)
+		pyglet.clock.schedule_interval(self.__update_simulation, 1/60.)
+		pyglet.app.run()
 		
-		if self.__mode_seekers == 'random':
-			seeker_team = team.RandomSeekerTeam(self.__num_seekers, seeker_map)
-
-		while hiders_caught < self.__num_hiders and time_step < self.__max_steps:
-			# update the percept of each robot
-
-
 
 
 
