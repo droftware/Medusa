9950fe0571af1e2654652875ca9cc64224fd8084
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-07-16 15:22:52 +0530

    Removed grid dependence in mapmanager class

--- hiseek/mapmanager.py
+++ hiseek/mapmanager.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-import grid
-import coord
+import gamemap
 
 class BasicMapManager(object):
 	'''
@@ -10,36 +9,7 @@ class BasicMapManager(object):
 	'''
 	def __init__(self, mapworld):
 		self.__map = mapworld
-		num_rows = self.__map.get_num_rows()
-		num_cols = self.__map.get_num_cols()
-		self.__heat_field = Grid(num_rows, num_cols)
-
-		# Prepare heat map
-		for i in range(num_rows):
-			for j in range(num_cols):
-				if self.__map.is_obstacle(i, j):
-					for a in range(1, 3):
-						for x in [-a, a]:
-							for y in [-a, a]:
-								row_pos = i + x
-								col_pos = j + y
-								if self.__map.is_inside(row_pos, col_pos):
-									if self.__map.is_free(row_pos, col_pos):
-										value = self.__heat_field.get(row_pos, col_pos) 
-										self.__heat_field.set(row_pos, col_pos, value + 1)
-
-	def get_heat_value(self, coordinate):
-		x, y = coordinate.get_tuple()
-		assert(self.__map.is_inside(x, y))
-		return self.__heat_field.get(x, y)
-
-	def is_inside(self, coordinate):
-		x, y = coordinate.get_tuple()
-		return self.__map.is_inside(x, y)
-
-	def is_obstacle(self, coordinate):
-		x, y = coordinate.get_tuple()
-		return self.__map.is_obstacle(x, y)
+		
 
 
 
