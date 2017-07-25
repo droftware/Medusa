d308d41275fc4c46890639a93dd375bdae1ec1d7
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-07-25 10:45:37 +0530

    Removed python3 enum Instead, simple mechanisms are used to achieve the same functionality and remove the python3 dependency.

--- hiseek/action.py
+++ hiseek/action.py
@@ -1,6 +1,5 @@
-import enum
 
-class Action(enum.Enum):
+class Action:
 	North = 1
 	NorthEast = 2
 	East = 3
@@ -11,5 +10,16 @@ class Action(enum.Enum):
 	NorthWest = 8
 	Stop = 9
 
+	all_actions = [North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest, Stop]
+	num_actions = len(all_actions)
 
+ROTATION = {}
+ROTATION[Action.North] = 270.001 
+ROTATION[Action.NorthEast] = 315.001
+ROTATION[Action.East] = 0.001
+ROTATION[Action.SouthEast] = 45.001
+ROTATION[Action.South] = 90.001
+ROTATION[Action.SouthWest] = 135.001
+ROTATION[Action.West] = 180.001
+ROTATION[Action.NorthWest] = 225.001
 
