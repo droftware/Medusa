b37531f75a6d653ed274fe61f2789c9008999e3b
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-07-26 20:30:10 +0530

    Handling cases in which closest intersect is not found. Added a few other support methods.

--- hiseek/gamemap.py
+++ hiseek/gamemap.py
@@ -14,11 +14,10 @@ class PolygonMap(object):
 		self.__polygons = []
 		self.__boundary_polygon = None
 		self.__all_polygons = None # Includes all the obstacle polygons as well as boundary polygon
-
-		map_name = 'id_' + str(map_id) + '.polygons'
-		# print('Path:', map_name)
-		assert(os.path.isfile(map_name))
-		f = open(map_name, 'r')
+		self.__map_name = 'id_' + str(map_id) + '.polygons'
+		# print('Path:', self.__map_name)
+		assert(os.path.isfile(self.__map_name))
+		f = open(self.__map_name, 'r')
 		first = True
 		for line in f:
 			line.strip()
@@ -30,6 +29,10 @@ class PolygonMap(object):
 
 					points_tuple = (0, 0, self.__width, 0, self.__width, self.__height, 0, self.__height)
 					self.__boundary_polygon = shapes.Polygon(points_tuple)
+
+					offset = 10
+					# points_tuple = (offset, offset, self.__width - offset, offset, self.__width - offset, self.__height - offset, offset, self.__height-offset)
+					# self.__imaginary_boundary = shapes.Polygon(points_tuple)
 					first = False
 				else:
 					points_list = [int(point) for point in points_list]
@@ -57,6 +60,17 @@ class PolygonMap(object):
 	def get_boundary_polygon(self):
 		return self.__boundary_polygon
 
+	def get_map_name(self):
+		return self.__map_name
+
+	def check_boundary_collision(self, position):
+		'''
+			Retusn True if point collides with the boundary
+		'''
+		if self.__boundary_polygon.is_point_inside(position):
+			return False
+		return True
+
 	def check_obstacle_collision(self, position):
 		'''
 			Returns True if point collides(is inside) any
@@ -68,9 +82,6 @@ class PolygonMap(object):
 				return True
 		return False
 
-	def to_radians(degrees):
-		return math.pi * degrees / 180.0
-
 	def get_visibility_polygon(self, current_position, current_rotation, num_rays, visibility_angle):
 		# c = coord.Coord(self.x, self.y)
 		vis_points = [int(current_position.get_x()), int(current_position.get_y())]
@@ -78,9 +89,14 @@ class PolygonMap(object):
 		rotation = current_rotation - visibility_angle
 		offset = (visibility_angle * 2.0)/num_rays
 		while rotation < current_rotation + visibility_angle:
-			rotation_x = math.cos(PolygonMap.to_radians(-rotation))
-			rotation_y = math.sin(PolygonMap.to_radians(-rotation))
+			rotation_x = math.cos(coord.Coord.to_radians(-rotation))
+			rotation_y = math.sin(coord.Coord.to_radians(-rotation))
 			r = coord.Coord(current_position.get_x() + rotation_x, current_position.get_y() + rotation_y)
+			rotation += offset
+			if r.get_x() < 0 or r.get_x() > self.__width or r.get_y() < 0 or r.get_y() > self.__height:
+				vis_points.append(int(current_position.get_x()))
+				vis_points.append(int(current_position.get_y()))
+				continue
 			ray = shapes.Line(current_position, r)
 			# print('ray:', ray)
 			closest_intersect = None
@@ -94,17 +110,20 @@ class PolygonMap(object):
 					if not closest_intersect or intersect[1] < closest_intersect[1]:
 						closest_intersect = intersect
 
-			# print(closest_intersect[0])
 			
-			assert(closest_intersect)
+
+			if not closest_intersect:
+				print('Closest intersect not found')
+				print('From coordinate:', current_position)
+				print('Ray:', ray)
+				print('Segment:', polygon.get_line(i))
+				continue
+
 			vis_points.append(int(closest_intersect[0].get_x()))
 			vis_points.append(int(closest_intersect[0].get_y()))
 
-			rotation += offset
+			
 
 		vis_points_tuple = tuple(vis_points)
 		visibility_polygon = shapes.Polygon(vis_points_tuple)
 		return visibility_polygon
-
-
-
