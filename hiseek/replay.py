bb265ef49e5e8465438d1ba2fda8fd62a6d317a5
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-08-24 12:14:18 +0530

    Changed the Graphics call signature

--- hiseek/replay.py
+++ hiseek/replay.py
@@ -6,7 +6,7 @@ import action
 
 class Replay(object):
 
-	def __init__(self, replay_file, fps, velocity, fixed_time_quanta):
+	def __init__(self, replay_file, conf_options):
 
 		self.__rf = open(replay_file, 'r')
 		self.__map_id = None
@@ -37,16 +37,17 @@ class Replay(object):
 		window_width = self.__map.get_map_width()
 		window_height = self.__map.get_map_height()
 
-		self.__fps = fps
-		self.__velocity = velocity
-		self.__fixed_time_quanta = fixed_time_quanta
+		self.__conf_options = conf_options
+		self.__fps = self.__conf_options.get_fps()
+		self.__velocity = self.__conf_options.get_velocity()
+		self.__fixed_time_quanta = self.__conf_options.get_fixed_time_quanta()
 
 		self.__inactive_hiders = []
 		self.__first_flag = {agent.AgentType.Hider:True, agent.AgentType.Seeker:True}
+	# def __init__(self, window_width, window_height, num_hiders, num_seekers, polygon_map, conf_options):
 
-
-		self.__window = graphics.Graphics(window_width, window_height, self.__num_hiders, self.__num_seekers, False, None, fps, velocity, self.__map, fixed_time_quanta)
-		print('graphics class initialized')
+		self.__window = graphics.Graphics(window_width, window_height, self.__num_hiders, self.__num_seekers, self.__map, self.__conf_options)
+		# print('graphics class initialized')
 
 
 	def __update_agents(self, agent_type, agent_data):
@@ -62,24 +63,22 @@ class Replay(object):
 					continue
 			solo_agent = agent_list[i].strip()
 			if solo_agent == 'X*X':
-				print('setting hider inactive')
+				# print('setting hider inactive')
 				self.__window.set_player_inactive(agent_type, i)
 				self.__inactive_hiders.append(i)
 			else:
 				solo_agent = solo_agent.split('*')
 				position = solo_agent[0].split(',')
-				print('position:', position)
-				print('hiders caught:', len(self.__inactive_hiders))
+				# print('position:', position)
+				# print('hiders caught:', len(self.__inactive_hiders))
 				x = int(float(position[0]))
 				y = int(float(position[1]))
 				act = action.Action.string2action[position[2]]
-				print('act:', act)
+				# print('act:', act)
 				if act != action.Action.ST:
 					rotation = action.ROTATION[act]
 					if not self.__first_flag[agent_type]:
 						self.__window.set_player_rotation_raw(agent_type, i, rotation)
-
-
 				
 				self.__window.set_player_position_raw(agent_type, i, x, y)
 				visibility_list = solo_agent[1].split(',')
@@ -103,5 +102,5 @@ class Replay(object):
 
 
 	def run_replay(self):
-		pyglet.clock.schedule_interval(self.update, 1/30.0) # update at 60Hz
+		pyglet.clock.schedule_interval(self.update, 1/self.__fps) # update at 60Hz
 		pyglet.app.run()
