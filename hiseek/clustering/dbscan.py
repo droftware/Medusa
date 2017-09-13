f3084820636909c4a4da4056c15826df868d7d2c
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-09-13 18:48:25 +0530

    Corrected DBSCAN Prior to this fix, the clustering was not being done correctly due to an error in code.

--- hiseek/clustering/dbscan.py
+++ hiseek/clustering/dbscan.py
@@ -1,12 +1,15 @@
 import numpy as np
 import copy
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 def get_distance(visibility_map, x1, y1, x2, y2):
 	return abs(visibility_map[x1, y1] - visibility_map[x2, y2])
 
 def check_legal(visibility_map, x, y):
-	if x >= 0 and x < visibility_map.shape[0]:
-		if y >= 0 and y < visibility_map.shape[1]:
+	if x > 0 and x < visibility_map.shape[0] - 1:
+		if y > 0 and y < visibility_map.shape[1] - 1:
 			if visibility_map[x, y] != -1:
 				return True
 	return False
@@ -22,7 +25,9 @@ def region_query(visibility_map, x, y, eps):
 			a, b = x + i, y + j
 			if check_legal(visibility_map, a, b):
 				if get_distance(visibility_map, a, b, x, y) < eps:
+					# print('appending', a,b, 'to neighbourhood - '),
 					neighbourhood.append((a,b))
+	# print()
 
 	return neighbourhood
 
@@ -41,28 +46,55 @@ def dbscan(file_name, eps, minPts):
 
 	for i in range(num_rows):
 		for j in range(num_cols):
+			if not check_legal(visibility_map, i, j):
+				continue
 			if label[i, j] == UNDEFINED:
+				print('')
+				print('Label:', i,j ,' is UNDEFINED')
 				neighbourhood = region_query(visibility_map, i, j , eps)
+				print('Its neighbours are:', neighbourhood)
 				if len(neighbourhood) + 1 < minPts:
 					label[i, j] = NOISE
 				else:
 					cluster += 1
 					label[i, j] = cluster
-					for neighbur in neighbourhood:
-						ni, nj = neighbur[0], neighbur[1] 
+					idx = 0
+					while idx < len(neighbourhood):
+						# print('*:', neighbourhood)
+						neighbour = neighbourhood[idx]
+						print('Exploring neighbour:', neighbour)
+						ni, nj = neighbour[0], neighbour[1] 
 						if label[ni, nj] == NOISE:
+							print('Its NOISE')
 							label[ni, nj] = cluster
 						if label[ni, nj] == UNDEFINED:
+							print('Assigning it to current cluster')
 							label[ni, nj] = cluster
 							nextNeighbourhood = region_query(visibility_map, ni, nj, eps)
+							print('Neighbours of:', neighbour, 'are:', nextNeighbourhood)
 							if len(nextNeighbourhood) >= minPts:
+								print('Appending neighbour\'s neighbours')
 								neighbourhood = neighbourhood + copy.deepcopy(nextNeighbourhood)
+						idx += 1
 
+	label = label.astype('int')
 	return label
 
 def main():
 	file_name = 'id_3.obstruction'
-	label = dbscan(file_name)
+	eps = 100
+	minPts = 8
+	label = dbscan(file_name, eps, minPts)
+	print('Clustering done')
+	np.savetxt(file_name.split('.')[0] + '.clusters', label)
+	print('File saved')
+	print(label)
+
+	f, ax = plt.subplots(figsize=(label.shape[0], label.shape[1]))
+	sns.heatmap(label, annot=True, fmt="d", linewidths=.5, ax=ax)
+	plt.savefig(file_name.split('.')[0]+ '_clusters')
+
+
 if __name__ == '__main__':
     main()
 
