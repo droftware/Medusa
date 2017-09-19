e45f493efcf41a9e63fad67c80c5ed1d36dee6f5
Akshat Tandon <akshat.tandon@research.iiit.ac.in>
2017-09-19 15:13:28 +0530

    Modified heatmap generator

--- hiseek/tools/heatmap_generator.py
+++ hiseek/tools/heatmap_generator.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-file_name = 'id_3.obstruction'
+file_name = 'id_4.obstruction'
 heat_map = np.loadtxt(file_name)
 heat_map = heat_map.astype('int')
 
@@ -13,4 +13,4 @@ num_cols = heat_map.shape[1]
 # Draw a heatmap with the numeric values in each cell
 f, ax = plt.subplots(figsize=(num_rows, num_cols))
 sns.heatmap(heat_map, annot=True, fmt="d", linewidths=.5, ax=ax)
-plt.savefig('obstruction')
+plt.savefig(file_name.split('.')[0] + '_' + file_name.split('.')[1] )
